15a5766d546033dc12febd703e09dff8fe651568
ghostfly23333 <ghostfly233@outlook.com>
2023-12-19 01:45:54 +0800

    add admin page

--- src/main.js
+++ src/main.js
@@ -6,15 +6,27 @@
 
 // Components
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
 
 // Composables
 import { createApp } from 'vue'
 
 // Plugins
 import { registerPlugins } from '@/plugins'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 registerPlugins(app)
 
-app.mount('#app')
+app.use(ElementPlus, {
+    locale: zhCn,
+    size: 'default',
+    zIndex: 3000,
+    })
+    .mount('#app')
